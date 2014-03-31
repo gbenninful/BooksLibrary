090ac439418132879846ff3e5659dc42082d9732
gbenninful <enninful@hotmail.com>
2014-03-30 18:24:07 -0700

    Posting to & Reading from Windows Azure

--- booksLib.js
+++ booksLib.js
@@ -1,40 +1,56 @@
-$(function(){
+$(function () {
     "use strict";
-	var Book = function (bookTitle, firstName, lastName, gender) {
-	    this.title = bookTitle;
-	    this.firstName = firstName;
-	    this.lastName = lastName;
-	    this.gender = gender;
-	};
-	var baseUrl = "https://georgeslibrary.azure-mobile.net/tables/authorInfo",
-		client = new WindowsAzure.MobileServiceClient(
-    					"https://georgeslibrary.azure-mobile.net/",
-    					"HleVTiVmsNsfQNEIXAMdRjfnnhudBg11");
-	
-	var book = {
-	    bookTitle: "Animal Farm",
-	    firstName: "George",
-	    lastName: "Orwell",
-        gender: "Male"
-	};
-	client.getTable("authorInfo").insert(book);
+
+    var MobileServiceClient = WindowsAzure.MobileServiceClient,
+        client = new MobileServiceClient("https://georgeslibrary.azure-mobile.net/", "HleVTiVmsNsfQNEIXAMdRjfnnhudBg11"),
+        authorInfoTable = client.getTable("authorInfo");
+
+    $("#bookform").submit(function (e) {
+
+        var bookTitle = $("#bookTitle").val(),
+            firstName = $("#firstName").val(),
+            lastName = $("#lastName").val(),
+            gender = $("#gender").val();
+
+        authorInfoTable.insert({
+            BookTitle: bookTitle,
+            FirstName: firstName,
+            LastName: lastName,
+            Gender: gender
+        })
+            .then(function (data) {
+                //console.log(data);
+                toastr.success("Your post was successful");
+            }, function (error) {
+                //console.log("The error is: " + error);
+                toastr.error("Your error is " + error);
+            })
+        .then($("#display").click(function () {
+
+            refreshauthorInfo();
+        }));
+
+        e.preventDefault();
+    });
+
+    function refreshauthorInfo() {
+
+        var query = authorInfoTable;
+        query.read().then(function (authors) {
+            var listAuthorInfo = $.map(authors, function (author) {
+                return $("<tr>")
+                    .attr("data-author-id", author.id)
+                    .append($("<td>").html(author.bookTitle))
+                    .append($("<td>").html(author.firstName))
+                    .append($("<td>").html(author.lastName))
+                    .append($("<td>").html(author.gender));
+            });
+
+            $("#authors").empty().append(listAuthorInfo);
+            $("#authors").prepend($("<th>Book Title</th> <th>First Name</th> <th>Last Name</th> <th>Gender</th>"));
 
 
-	//$("#save").click(function (event) {
-	//    var bookTitle = $("#bookTitle").val();
-	//    var firstName = $("firstName").val();
-	//    var lastName = $("lastName").val();
-	//    var gender = $("gender").val();
+        })
+    }
 
-	//    event.preventDefault();
-	//});
-
-	//var book = new Book(bookTitle, firstName, lastName, gender);
-    
-	//client.getTable("authorInfo").insert(book);
-	    
-	
-	
-	
-	
 }());
