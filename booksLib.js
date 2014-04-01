4e74cdda1e402050050afbc70f945603d607dd4c
gbenninful <enninful@hotmail.com>
2014-04-01 11:36:08 -0700

    Added Update & Delete functionalities

--- booksLib.js
+++ booksLib.js
@@ -5,18 +5,31 @@ $(function () {
         client = new MobileServiceClient("https://georgeslibrary.azure-mobile.net/", "HleVTiVmsNsfQNEIXAMdRjfnnhudBg11"),
         authorInfoTable = client.getTable("authorInfo");
 
+    var title = $("#bookTitle"),
+         bookTitle = title.val(),
+         fName = $("#firstName"),
+          firstName = fName.val(),
+         lName = $("#lastName"),
+         lastName = lName.val(),
+         gen = $("#gender"),
+         gender = gen.val();
+
     $("#bookform").submit(function (e) {
 
-        var bookTitle = $("#bookTitle").val(),
-            firstName = $("#firstName").val(),
-            lastName = $("#lastName").val(),
-            gender = $("#gender").val();
+        //var title = $("#bookTitle"),
+        //    bookTitle = title.val(),
+        //    fName = $("#firstName"),
+        //     firstName = fName.val(),
+        //    lName = $("#lastName"),
+        //    lastName = lName.val(),
+        //    gen = $("#gender"),
+        //    gender = gen.val();
 
         authorInfoTable.insert({
-            BookTitle: bookTitle,
-            FirstName: firstName,
-            LastName: lastName,
-            Gender: gender
+            bookTitle: bookTitle,
+            firstName: firstName,
+            lastName: lastName,
+            gender: gender
         })
             .then(function (data) {
                 //console.log(data);
@@ -24,33 +37,71 @@ $(function () {
             }, function (error) {
                 //console.log("The error is: " + error);
                 toastr.error("Your error is " + error);
-            })
-        .then($("#display").click(function () {
+            });
+     
 
-            refreshauthorInfo();
-        }));
+        title.val("").focus();
+        fName.val("");
+        lName.val("");
+        gen.val("");
 
         e.preventDefault();
     });
 
-    function refreshauthorInfo() {
+    function refreshAuthorInfo() {
 
         var query = authorInfoTable;
         query.read().then(function (authors) {
+            var counter = 0;
             var listAuthorInfo = $.map(authors, function (author) {
+                counter += 1;
+
                 return $("<tr>")
                     .attr("data-author-id", author.id)
-                    .append($("<td>").html(author.bookTitle))
-                    .append($("<td>").html(author.firstName))
-                    .append($("<td>").html(author.lastName))
-                    .append($("<td>").html(author.gender));
+                    .append($("<td>").html(counter))
+                    .append($("<td>").html(author.bookTitle).attr("class", "row-title"))
+                    .append($("<td>").html(author.firstName).attr("class", "fname"))
+                    .append($("<td>").html(author.lastName).attr("class", "lname"))
+                    .append($("<td>").html(author.gender).attr("class", "gen"))
+                    .append($("<button class='btn btn-danger author-delete'>Delete</button>"))
+                    .append($("<button class='btn btn-warning author-edit'>Edit</button>"));
             });
 
             $("#authors").empty().append(listAuthorInfo);
-            $("#authors").prepend($("<th>Book Title</th> <th>First Name</th> <th>Last Name</th> <th>Gender</th>"));
-
-
-        })
+            $("#summary").html("Total number of Books in George's Library: <strong>" + authors.length + '</strong>');
+            $("#authors").prepend($("<th>Shelf</th> <th>Book Title</th> <th>First Name</th> <th>Last Name</th> <th>Gender</th>"));
+        });
     }
+
+    //Delete
+    function getAuthorInfoId(formElement) {
+
+        return $(formElement).closest("tr").attr("data-author-id");
+    };
+
+    $(document.body).on("click", ".author-delete", function () {
+        authorInfoTable.del({ id: getAuthorInfoId(this) }).then(refreshAuthorInfo);
+    });
+
+    $("#display").click(function () {
+
+        refreshAuthorInfo();
+    })
+
+    //Updates
+    $(document.body).on('click', '.author-edit', function () {
+        //get the id, title, etc from that row
+        var idToUpdate = getAuthorInfoId(this);
+        var titleToUpdate = $(this).closest("tr").find(".row-title").html();
+
+        title.val(titleToUpdate);
+
+        //populate the text boxes with that data
+
+        //post/update
+        $(document.body).on('click', '#btn-update', function () {
+            authorInfoTable.update({ id: idToUpdate, bookTitle: title.val() }).then(refreshAuthorInfo);
+        });
+    })
 
 }());
